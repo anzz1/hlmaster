0e4e367e85e5e9c29648207958847bbc220c1860
anzz1 <anzz1@live.com>
2024-02-05 10:52:03 +0200

    linux optimization
    disable stack canary and ASLR nonsense

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f ./hlmaster ]; then
   echo "Compiling hlmaster ..."
-  gcc -O2 -w -march=native -fvisibility=hidden -fomit-frame-pointer hlmaster.c -o hlmaster
+  gcc -O2 -w -march=native -fvisibility=hidden -fomit-frame-pointer -fno-stack-protector -fno-pie -no-pie hlmaster.c -o hlmaster
   chmod +x ./hlmaster
 fi
 
